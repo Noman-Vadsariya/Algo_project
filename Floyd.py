b8b14077bd8b37f6534b2172e25d7f23a2443cd2
Noman-Vadsariya <noman.nooruddin02@gmail.com>
2021-12-03 21:39:37 +0500

    All Algos Done

--- Floyd.py
+++ Floyd.py
@@ -1,25 +1,27 @@
 from graph import Graph
 
+
 def FloydWarshall(G):
-    dist = [[float("Inf")]*G.V]*G.V
+    dist = [[float("Inf") for i in range(G.V)] for j in range(G.V)]
 
     for i in range(G.V):
-        dist[i][i] = 0;
-        temp = G.graph[i];
-        while (temp is not None):
-            dist[i][temp.vertex] = temp.weight;
-            temp = temp.next;
+        dist[i][i] = 0
+        temp = G.graph[i]
+        while temp is not None:
+            dist[i][temp.vertex] = temp.weight
+            temp = temp.next
 
-    for k in range(G.V):   #for considering each vertex as a source
+    for k in range(G.V):  # for considering each vertex as a source
         for i in range(G.V):
             for j in range(G.V):
-                if (dist[i][j] > dist[i][k] + dist[k][j]):
-                    dist[i][j] = dist[i][k] + dist[k][j];
+                if dist[i][j] > dist[i][k] + dist[k][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
 
     for i in range(G.V):
         for j in range(G.V):
-            print("{} ".format(dist[i][j]),end="")
+            print("{}   ".format(dist[i][j]), end="")
         print()
+
 
 if __name__ == "__main__":
     V = 9
